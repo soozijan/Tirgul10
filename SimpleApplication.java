8044903155d743c5e29f0cbe29230c031739b963
soozijan <75090137+soozijan@users.noreply.github.com>
2021-05-23 18:54:13 +0300

    Create SimpleApplication.java

---
+++ SimpleApplication.java
@@ -0,0 +1,13 @@
+package com.example.Simple1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Simple1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Simple1Application.class, args);
+	}
+
+}
